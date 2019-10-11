c363414f419ae6be88b917e59bf1db303dd0d21a
sharathgowdam <56424916+sharathgowdam@users.noreply.github.com>
2019-10-11 15:57:38 +0530

    Update userReducer.js

--- reduxdemo/src/reducers/userReducer.js
+++ reduxdemo/src/reducers/userReducer.js
@@ -1,12 +1,12 @@
 export default () => [
     {
         "id": 1,
-        "firstName":"Naveen b",
+        "firstName":"Pooja",
         "details":"1....is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
     },
     {
         "id": 2,
-        "firstName":"Relyon",
+        "firstName":"Sharath",
         "details":"2....is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
     }
 
